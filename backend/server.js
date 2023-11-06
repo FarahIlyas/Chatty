ef78d40f48fb6072a3357e1af7458786e5f9c4e4
Muhammad Abdullah Mushtaq <abdullahmushtaq08@gmail.com>
2023-11-06 23:59:35 +0500

    DB connected

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './db/connectDB.js';
 
 dotenv.config(); // allows to see the content inside .env file
+
+connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
