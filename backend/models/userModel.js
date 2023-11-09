228149369113bb4758907b892e991fe4568de270
Muhammad Abdullah Mushtaq <abdullahmushtaq08@gmail.com>
2023-11-09 14:49:19 +0500

    models created

---
+++ backend/models/userModel.js
@@ -0,0 +1,49 @@
+//Models is kind of like a blueprint which fields you are gona have inside a model. it is like a table in sql.
+
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    username: {
+    type: String,
+    required: true,
+    unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        minLength: 6,
+        required: true,
+    },
+    profilePic: {
+        type: String,
+        default: "",
+    },
+    followers: {
+        type: [String],
+        default: [],
+    },
+    following: {
+        type: [String],
+        default: [],
+    },
+    bio: {
+        type: String,
+        default: "",
+    }
+}, {
+    timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+// it will create users in your database
+// we will call the 'User' and use "userSchema" to create a new user.
+
+export default User;
