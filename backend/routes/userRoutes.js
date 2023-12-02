3d09c218c2a45a17ec8ecda863c716f11a4a1768
Muhammad Abdullah Mushtaq <abdullahmushtaq08@gmail.com>
2023-12-02 16:32:56 +0500

    updatedprofile and getprofile end points done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { followUnFollowUser, loginUser, logoutUser, signupUser } from "../controllers/userController.js";
+import { followUnFollowUser, getUserProfile, loginUser, logoutUser, signupUser, updateUser } from "../controllers/userController.js";
 import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
 //we will create controllers separately to handle each routes. it is best practice.
 
+router.get("/profile/:username", getUserProfile);
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
@@ -13,5 +14,6 @@ router.post("/follow/:id", protectRoute, followUnFollowUser);     // it is going
 // adding protectRoute() as middleware to protect the route. adding this to ensure that no one can follow/unfollow anybody if they are not loggedin.
 // if i am not loggedin i can't update my profile. 
 // when we enter above route this will first run the middleware and upon succes it'll run the followUnFollowUser() with help of next() in middleware.
+router.post("/update/:id", protectRoute, updateUser);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import userRoutes from "./routes/userRoutes.js";
 dotenv.config(); // allows to see the content inside .env file
 
 connectDB();
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
